17067c3a934a9329be1b8d3e822655f5d270389f
pjl14 <pankajexpressian@gmail.com>
2025-07-06 17:08:19 +0530

    updated the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "PJ",
+  title: "API Developer & Content Creator",
+  email: "pj@world.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
