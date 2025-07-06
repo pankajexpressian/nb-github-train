31fe086c7741fd0492c8d72089871f8a892c8f96
pjl14 <pankajexpressian@gmail.com>
2025-07-06 17:21:40 +0530

    Added the event listenert to the down arrow button. Page is now scrolling on click

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
@@ -22,18 +22,56 @@ import PropTypes from "prop-types";
  */
 import image from "../images/woman-with-tablet.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText =
+  "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
+  React.useEffect(() => {
+    const arrow = document.getElementById("scroll-arrow");
+    if (arrow) {
+      arrow.addEventListener("click", () => {
+        const nextSection = document.getElementById("about");
+        if (nextSection) {
+          nextSection.scrollIntoView({ behavior: "smooth" });
+        }
+      });
+    }
+    // Cleanup
+    return () => {
+      if (arrow) {
+        arrow.replaceWith(arrow.cloneNode(true));
+      }
+    };
+  }, []);
+
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "5rem",
+          left: "2rem",
+          width: "17rem",
+        }}
+      >
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      <div
+        style={{
+          position: "absolute",
+          bottom: "3rem",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <img
+          src={arrowSvg}
+          style={{ height: "3rem", width: "3rem", cursor: "pointer" }}
+          alt={imageAltText}
+          id="scroll-arrow"
+        />
       </div>
     </section>
   );
